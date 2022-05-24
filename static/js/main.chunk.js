0e1d4d8e9e6d37cd2ae2d02d440e23bf3fa2b8ee
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-05-25 00:52:04 +0200

    Update main.chunk.js

--- static/js/main.chunk.js
+++ static/js/main.chunk.js
@@ -17,9 +17,9 @@
                     c = n(3),
                     o = n(30),
                     i = n(62),
-                    u = new o.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
-                    s = new o.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
-                    p = new o.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
+                    u = new o.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
+                    s = new o.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
+                    p = new o.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
                     b = function() {
                         var e = Object(c.a)(a.a.mark((function e(t, n, r) {
                             var o, i, u, s, p, b = arguments;
@@ -447,7 +447,7 @@
                                             }
                                             return t.abrupt("return");
                                         case 2:
-                                            if (n = new k.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"), !H) {
+                                            if (n = new k.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"), !H) {
                                                 t.next = 15;
                                                 break
                                             }
@@ -578,9 +578,9 @@
                 I = n(112),
                 C = n(336),
                 W = n(597),
-                N = new M.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
-                D = new M.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
-                Y = new M.d.PublicKey("DupeRtRdTNeGYo18PcXyxxFWy7dC2YCzdkt1CJxLLfpo"),
+                N = new M.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
+                D = new M.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
+                Y = new M.d.PublicKey("D4rkEXTwE4zrTSRoS8bEiauVex24qmeHet7ekBbDepzk"),
                 z = "mainnet-beta",
                 _ = new M.d.Connection("https://api.mainnet-beta.solana.com/"),
                 U = parseInt("1635893271", 10),
